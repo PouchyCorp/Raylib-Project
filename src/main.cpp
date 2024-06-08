46f15a140caff6cebf280106a2e7f6f94536ffc6
PouchyCorp <pouchy.l.acarien@gmail.com>
2024-06-08 17:06:19 +0100

    trucs

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,20 @@
 #include <raylib.h>
 #include <iostream>
 #include <array>
-#include <cstdlib> // For malloc and free
+#include <algorithm> // For std::copy
 
 using namespace std;
 
 void copyArray(int *source, int *destination, int totalSize)
 {
-    for (int i = 0; i < totalSize; ++i)
-    {
-        *(destination + i) = *(source + i);
-    }
+    std::copy(source, source + totalSize, destination);
 }
 
 array<int, 2> coordToGrid(int coordX, int coordY, int sizeX, int sizeY)
 {
     array<int, 2> gridCoord;
-    gridCoord[1] = (coordX - (coordX % (GetScreenHeight() / sizeY))) / (GetScreenHeight() / sizeY);
-    gridCoord[0] = (coordY - (coordY % (GetScreenWidth() / sizeX))) / (GetScreenWidth() / sizeX);
+    gridCoord[1] = coordX / (GetScreenWidth() / sizeX);
+    gridCoord[0] = coordY / (GetScreenHeight() / sizeY);
     return gridCoord;
 }
 
@@ -25,33 +22,25 @@ int main()
 {
     Color black = Color{0, 0, 0, 255};
     Color white = Color{255, 255, 255, 255};
-    Color gray = Color{10, 10, 10, 255};
+    //Color gray = Color{10, 10, 10, 255};
 
-    const int screenWidth = 800;
-    const int screenHeight = 800;
+    const int screenWidth = 900;
+    const int screenHeight = 900;
 
     srand(time(NULL));
 
-    const int gridSizeX = 500;
-    const int gridSizeY = 500;
+    const int gridSizeX = 300;
+    const int gridSizeY = 300;
     const int gridCellSizeX = screenWidth / gridSizeX;
     const int gridCellSizeY = screenHeight / gridSizeY;
 
-    int* grid = new int[gridSizeX * gridSizeY]();
-    int* fgrid = new int[gridSizeX * gridSizeY]();
-    
-    for (int x = 0; x < gridSizeX; x++)
-    {
-        for (int y = 0; y < gridSizeY; y++)
-        {
-            grid[y * gridSizeX + x] = 0;
-        }
-    }
+    int *grid = new int[gridSizeX * gridSizeY]();
+    int *fgrid = new int[gridSizeX * gridSizeY]();
 
     grid[20 * gridSizeX + 20] = 1;
 
     InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(1000);
+    SetTargetFPS(500);
     bool clicking = false;
 
     for (int i = 0; i <= 50; i++)
@@ -77,13 +66,16 @@ int main()
         if (0 < mousePos.x && mousePos.x < screenWidth && 0 < mousePos.y && mousePos.y < screenHeight && clicking)
         {
             array<int, 2> gridPos = coordToGrid(mousePos.x, mousePos.y, gridSizeX, gridSizeY);
-            fgrid[gridPos[0] * gridSizeX + gridPos[1]] = 1;
+            if (gridPos[0] < gridSizeY && gridPos[1] < gridSizeX)
+            {
+                fgrid[gridPos[0] * gridSizeX + gridPos[1]] = 1;
+            }
         }
 
         // gravity
         for (int x = 0; x < gridSizeX; x++)
         {
-            for (int y = 0; y < gridSizeY; y++)
+            for (int y = gridSizeY - 1; y >= 0; y--)
             {
                 if (grid[y * gridSizeX + x] == 1 && y + 1 < gridSizeY)
                 {
@@ -92,31 +84,19 @@ int main()
                         fgrid[y * gridSizeX + x] = 0;
                         fgrid[(y + 1) * gridSizeX + x] = 1;
                     }
-                    else if ((grid[(y + 1) * gridSizeX + x + 1] == 0 || grid[(y + 1) * gridSizeX + x - 1] == 0) && (x + 1 < gridSizeX && x - 1 >= 0))
+                    else if ((x + 1 < gridSizeX && grid[(y + 1) * gridSizeX + x + 1] == 0) ||
+                             (x - 1 >= 0 && grid[(y + 1) * gridSizeX + x - 1] == 0))
                     {
                         fgrid[y * gridSizeX + x] = 0;
 
-                        if (grid[(y + 1) * gridSizeX + x + 1] == 0 && grid[(y + 1) * gridSizeX + x - 1] == 0)
+                        if (x + 1 < gridSizeX && grid[(y + 1) * gridSizeX + x + 1] == 0 &&
+                            (x - 1 < 0 || grid[(y + 1) * gridSizeX + x - 1] != 0))
                         {
-                            if (rand() % 2 == 1)
-                            {
-                                fgrid[(y + 1) * gridSizeX + x + 1] = 1;
-                            }
-                            else
-                            {
-                                fgrid[(y + 1) * gridSizeX + x - 1] = 1;
-                            }
+                            fgrid[(y + 1) * gridSizeX + x + 1] = 1;
                         }
-                        else
+                        else if (x - 1 >= 0 && grid[(y + 1) * gridSizeX + x - 1] == 0)
                         {
-                            if (grid[(y + 1) * gridSizeX + x + 1] != 0)
-                            {
-                                fgrid[(y + 1) * gridSizeX + x - 1] = 1;
-                            }
-                            else
-                            {
-                                fgrid[(y + 1) * gridSizeX + x + 1] = 1;
-                            }
+                            fgrid[(y + 1) * gridSizeX + x - 1] = 1;
                         }
                     }
                 }
@@ -125,7 +105,7 @@ int main()
 
         BeginDrawing();
         ClearBackground(black);
-
+        /*
         // grid ui
         for (int x = 0; x <= screenWidth; x += gridCellSizeX)
         {
@@ -135,13 +115,13 @@ int main()
         {
             DrawLine(0, y, screenWidth, y, gray);
         }
-
+        */
         copyArray(fgrid, grid, gridSizeX * gridSizeY);
 
         // draw cells
-        for (int x = 0; x < gridSizeX; x++)
+        for (int y = 0; y < gridSizeY; y++)
         {
-            for (int y = 0; y < gridSizeY; y++)
+            for (int x = 0; x < gridSizeX; x++)
             {
                 if (grid[y * gridSizeX + x] == 1)
                 {
@@ -149,6 +129,7 @@ int main()
                 }
             }
         }
+
         EndDrawing();
     }
 
